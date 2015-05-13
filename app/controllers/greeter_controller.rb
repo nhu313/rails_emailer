dd1303ee3e26e849e653e9c81abffe860a211276
Nhu Nguyen <nhu313@gmail.com>
2015-05-12 23:19:22 -0500

    Connect emailer to send simple message

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -3,6 +3,7 @@ class GreeterController < ApplicationController
   end
 
   def sendmail
+    Emailer.send_message(params[:email], params[:subject], params[:message]).deliver_now
     render :text => "Message sent."
   end
 end
--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,2 +1,8 @@
 class Emailer < ApplicationMailer
+
+  def send_message(recipient, subject, message)
+    mail(to: recipient, subject: subject) do |format|
+      format.text { render text: message }
+    end
+  end
 end
