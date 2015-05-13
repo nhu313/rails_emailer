826355e42b5076be71a7f573738a04195b4a036b
Nhu Nguyen <nhu313@gmail.com>
2015-05-12 23:17:49 -0500

    Add email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :enable_starttls_auto => true,
+    :user_name => "my_email@gmail.com",
+    :password  => "my_mandrill_API_key",
+    :authentication => 'login',
+    :domain => 'example.com',
+  }
 end
